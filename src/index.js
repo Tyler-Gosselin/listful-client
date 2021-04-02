ea74ee0ead6df0bb339d8081b9c49f060ac9b884
Daniel Gosselin <dangoscode@gmail.com>
2021-04-02 16:25:41 -0600

    Update styles, Add auth stuff

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./Components/App";
+import AuthProvider from "./Components/Providers/AuthProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
